fe98f8591c57b8475c0da1579f7299ea7514e4ea
VLeo1966 <148269734+VLeo1966@users.noreply.github.com>
2024-09-21 04:14:43 +0300

    end dz2

--- zerocoderdz2/main/views.py
+++ zerocoderdz2/main/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 
 def home_view(request):
-    return render(request, 'home.html')
+    return render(request, 'main/home.html')
+def home_view(request):
+    return render(request, 'main/home.html')
 
 def about_view(request):
-    return render(request, 'about.html')
+    return render(request, 'main/about.html')
 
 def services_view(request):
-    return render(request, 'services.html')
+    return render(request, 'main/services.html')
 
 def contact_view(request):
-    return render(request, 'contact.html')
+    return render(request, 'main/contact.html')
