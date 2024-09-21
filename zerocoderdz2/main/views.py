e2ac7e4d816dd3efbeb6ec18787ec3bcf7856c0c
VLeo1966 <148269734+VLeo1966@users.noreply.github.com>
2024-09-21 04:37:38 +0300

    Update views.py

--- zerocoderdz2/main/views.py
+++ zerocoderdz2/main/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 
 def home_view(request):
     return render(request, 'main/home.html')
-def home_view(request):
-    return render(request, 'main/home.html')
 
 def about_view(request):
     return render(request, 'main/about.html')
